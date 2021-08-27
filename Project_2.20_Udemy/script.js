b2b0e660844437c88d325454c12dbdecb7a0bab2
rustemv <rustemv@gmail.com>
2021-08-27 14:43:16 +0300

    new @work

---
+++ Project_2.20_Udemy/script.js
@@ -0,0 +1,38 @@
+const options = {
+    name: 'test',   //свойства
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red',
+    },
+    makeTest: function() {      // метод
+        console.log('test message'); 
+    }
+    
+}
+
+/* console.log(options.name);
+
+console.log(options['name']);
+
+delete options.name; */
+
+console.log(options['colors']['border']);
+
+let counter = 0;
+for (let key in options) {
+        counter++;
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение: ${options[key][i]}`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение: ${options[key]}`);
+    }
+}
+
+console.log(Object.keys(options).length);
+
+options.makeTest();
+
